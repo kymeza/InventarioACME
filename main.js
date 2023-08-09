52fe0aef11e77443b568e73b1d9a3aae608864b6
Kyron Meza <kyron.meza@outlook.com>
2023-08-09 17:18:54 -0400

    CRUD inventario

--- main.js
+++ main.js
@@ -1,35 +1,182 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const session = require('express-session');
 const crypto = require('crypto');
 
-let db = new sqlite3.Database("database.db");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
-process.env["salt"] = fs.readFileSync("secret.txt", "utf-8").trim();
-console.log(process.env.salt);
+const path = require('path');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
+
+//Creamos una BaseDatos SQLite para testear
+//let db = new sqlite3.Database("database.db");
+let db = new sqlite3.Database(':memory:');
+
+
+//Leemos el secret.txt y manejamos cualquier exepcion.
+try {
+    process.env["salt"] = fs.readFileSync("secret.txt", "utf-8").trim();
+    console.log(process.env.salt);
+} catch (err) {
+    console.log('Error: ', err);
+}
+
+//INICIALIZAMOS BASE DE DATOS CON DATOS DE EJEMPLO -- RUN ONCE ONLY
 
 db.serialize(function() {
     db.run("CREATE TABLE usuarios (email TXT PRIMARY KEY, nombre TEXT, password TEXT)");
     db.run("CREATE TABLE objetos (id_objeto TXT PRIMARY KEY, desc_item TEXT, cantidad INTEGER)");
-})
 
-
-//PASSWORDS:
-//PASSWORD + SALT
-
-db.serialize(function() {
+    //Creamos Usuarios de Ejemplo
+    //PASSWORDS: PASSWORD + SALT
+    
     db.run("INSERT INTO usuarios VALUES ('admin@admin.cl','Admin Admin','f78ddd9b2cc49c49cc4696552fbc9f422bec0e56a71ea70bb2599b10107a4b5b')");
     db.run("INSERT INTO usuarios VALUES ('user@user.cl','User Usuario','61f5f2fa2e80f96b959b1688d6d3b0b242190daad54226629dc337aaf6ca1ba8')");
 })
 
+
+//Consultamos la DB para probar que existan los registros.
 db.all("SELECT * FROM usuarios", (err, rows ) => {
     console.log(rows); 
 });
 
+//inicializamos la app con Express
+let app = express(); 
 
+//Usaremos el bodyParser.json() para recepcionar/enviar datos en formato JSON
+app.use(bodyParser.json());
+//Usaremos el bodyParser.json() para recepcionar/enviar datos en formato JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Definimos un directorio con contenido estático; para archivos.
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+//Como usaremos arquetipo MVC, usaremos ejs para renderizar las vistas.
+app.set('view engine','ejs');
+
+//Usaremos cookieParser() para poder leer y modificar cookies (de sesión y/o personalizadas)
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.salt,
+    resave: false,
+    cookie: {secure: false}
+}));
+
+//Vista de Login
+app.get('/login', (req,res) => {
+    res.render("login");
+});
+
+//Servicio que se hace cargo del login del usuario
+app.post('/login', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    // First, check if the email exists
+    let sqlCheckEmail = "SELECT * FROM usuarios WHERE email = '" + email + "'";
+
+    db.get(sqlCheckEmail, [], (err, row) => {
+        if (err) {
+            res.send(err.message);
+            return;
+        }
+
+        if (row) {
+            if (row.password === password) {
+                req.session.email = email;
+                req.session.nombre = row.nombre
+                res.redirect('/');
+            } else {
+                res.send("Incorrect password.");
+            }
+        } else {
+            res.send("Email not found.");
+        }
+    });
+});
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/inventario', (req, res) => {
+    let sql = "SELECT * FROM objetos";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            res.send(err,err.message, err.stack)
+            return;
+        }
+        res.render("inventario/index", { objetos: rows });
+    });
+});
+
+app.get('/inventario/create', (req, res) => {
+    res.render('inventario/create');
+});
+
+app.get('/inventario/update', (req, res) => {
+    let sql = "SELECT id_objeto FROM objetos";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.render('inventario/update', { ids: rows });
+    });
+});
+
+app.get('/inventario/delete', (req, res) => {
+    let sql = "SELECT id_objeto FROM objetos";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.render('inventario/delete', { ids: rows });
+    });
+});
+
+// Handling item creation
+app.post('/inventario/create', (req, res) => {
+    let sql = `INSERT INTO objetos (id_objeto, desc_item, cantidad) VALUES ('${req.body.id_objeto}', '${req.body.desc_item}', ${req.body.cantidad})`;
+    db.run(sql, function(err) {
+        if (err) {
+            return res.status(500).send("Error inserting into database");
+        }
+        res.redirect('/inventario');
+    });
+});
+
+// Handling item update
+app.post('/inventario/update', (req, res) => {
+    let sql = `UPDATE objetos SET desc_item = '${req.body.desc_item}', cantidad = ${req.body.cantidad} WHERE id_objeto = '${req.body.id_objeto}'`;
+    db.run(sql, function(err) {
+        if (err) {
+            return res.status(500).send("Error updating the database");
+        }
+        res.redirect('/inventario');
+    });
+});
+
+// Handling item deletion
+app.post('/inventario/delete', (req, res) => {
+    let sql = `DELETE FROM objetos WHERE id_objeto = '${req.body.id_objeto}'`;
+    db.run(sql, function(err) {
+        if (err) {
+            return res.status(500).send("Error deleting from the database");
+        }
+        res.redirect('/inventario');
+    });
+});
+
+
+app.listen(9000);
 
 
 
